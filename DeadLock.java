eab6b823b719eee751a5a9ebffaf3d95500912c6
kavyasingh03 <kavyasingh.cs23@bmsce.ac.in>
2024-12-02 21:19:43 +0530

    DeadLock.java

--- DeadLock.java
+++ DeadLock.java
@@ -1,102 +1,59 @@
 class A {
+    synchronized void foo(B b) {
+        String name = Thread.currentThread().getName();
+        System.out.println(name + " entered A.foo");
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            System.out.println("A Interrupted");
+        }
+        System.out.println(name + " trying to call B.last()");
+        b.last();
+    }
 
-synchronized void foo(B b) {
-
-String name =
-Thread.currentThread().getName();
-
-System.out.println(name + " entered A.foo");
-
-try {
-
-Thread.sleep(1000);
-
-} catch(Exception e) {
-
-System.out.println("A Interrupted");
-
-}
-
-System.out.println(name + " trying to call B.last()");
-
-b.last();
-
-}
-
-void last() {
-
-System.out.println("Inside A.last");
-
-}
-
+    void last() {
+        System.out.println("Inside A.last");
+    }
 }
 
 class B {
+    synchronized void bar(A a) {
+        String name = Thread.currentThread().getName();
+        System.out.println(name + " entered B.bar");
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            System.out.println("B Interrupted");
+        }
 
-synchronized void bar(A a) {
+        System.out.println(name + " trying to call A.last()");
+        a.last();
+    }
 
-String name =
-Thread.currentThread().getName();
-
-System.out.println(name + " entered B.bar");
-
-try {
-
-Thread.sleep(1000);
-
-} catch(Exception e) {
-
-System.out.println("B Interrupted");
-
+    void last() {
+        System.out.println("Inside B.last");
+    }
 }
 
-System.out.println(name + " trying to call A.last()");
+class Deadlock implements Runnable {
+    A a = new A();
+    B b = new B();
 
-a.last();
+    Deadlock() {
+        Thread.currentThread().setName("MainThread");
+        Thread t = new Thread(this, "RacingThread");
+        t.start();
 
+        a.foo(b);
+        System.out.println("Back in mainthread");
+    }
+    public void run() {
+        b.bar(a);
+        System.out.println("Back in otherthread");
+    }
+    public static void main(String args[]) {
+        System.out.println("Name: Kavya Singh, USN: 1BM23CS146");
+        new Deadlock();
+    }
 }
 
-void last() {
-
-System.out.println("Inside A.last");
-
-}
-
-}
-
-class Deadlock implements Runnable
-{
-
-A a = new A();
-
-B b = new B();
-
-Deadlock() {
-
-Thread.currentThread().setName("MainThread");
-
-Thread t = new Thread(this,"RacingThread");
-
-t.start();
-
-a.foo(b); // get lock on a in this thread.
-
-System.out.println("Back in mainthread");
-
-}
-
-public void run() {
-
-b.bar(a); // get lock on b in otherthread.
- 
-System.out.println("Back in otherthread");
-
-}
-
-public static void main(String args[]) {
-System.out.println("Name: Likith T, USN: 1BM23CS171");
-new Deadlock();
-
-}
-
-}
